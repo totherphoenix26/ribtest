5a6442b8796de937c0d42682820a1198b64e4765
Rameez Samsodien <rameezs@allangray.co.za>
2022-02-01 14:16:24 +0200

    none: updated name

--- test/__test__/paint.spec.ts
+++ test/__test__/paint.spec.ts
@@ -3,12 +3,11 @@ import TestEnvironment from '../TestEnvironment'
 
 const fileTab = By2.nativeName('File tab');
 const open = By2.nativeName('Open');
-const image = By2.nativeName('TestingImage.png');
+const image = By2.nativeName('docornote.jpg');
 const openImage = By2.nativeAccessibilityId('1');
 const text = By2.nativeName('Text');
 const element  = By2.nativeClassName('Afx:2d0000:8');
 const textInput = By2.nativeClassName('RICHEDIT50W');
-const yellowSelector = By2.nativeAccessibilityId('23212');
 
 beforeAll(() => {
 return TestEnvironment.setup();;
@@ -25,13 +24,6 @@ await openImage.click();
 await text.click();
 await element.click();
 await textInput.sendKeys('test');
-await yellowSelector.click();
 
-
-//await open.click();
-//await plusButton.click();
-//await sevenButton.click();
-//await equalButton.click();
-//expect(await calculatorResult.getText()).toBe('Display is 8');
 })
 });
